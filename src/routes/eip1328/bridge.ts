c467741a689ee6e41e233874de13bc85aea11f2a
Rodrigo Couto <r@rodg.co>
2022-03-11 09:26:47 -0300

    clean up old stuff

--- src/routes/eip1328/bridge.ts
+++
@@ -1,9 +0,0 @@
-import type { EndpointOutput, RequestEvent } from '@sveltejs/kit';
-
-export async function get({ request, url, params }: RequestEvent): Promise<EndpointOutput> {
-	console.log('Request', request, '\nParams', params, '\nURL', url);
-
-	return {
-		status: 200
-	};
-}
