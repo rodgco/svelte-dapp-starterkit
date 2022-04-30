a36d14fc9033bd04bf4f702ccc3b95718ca3dc80
Rodrigo Couto <r@rodg.co>
2022-04-30 02:16:32 -0300

    implement changeNetwork in jsonrpc wallet

--- packages/svelther/src/lib/wallet_jsonrpc.ts
+++ packages/svelther/src/lib/wallet_jsonrpc.ts
@@ -1,5 +1,6 @@
 import Wallet from './wallet';
-import type { IOptions } from './wallet';
+import type { INetwork, IOptions } from './wallet';
+import networks from './networks.json';
 
 import { ethers } from 'ethers';
 
@@ -13,7 +14,35 @@ export default class JsonRpcWallet extends Wallet<ethers.providers.JsonRpcProvid
 		this.provider = <ethers.providers.JsonRpcProvider>(
 			ethers.getDefaultProvider(this.network.rpcUrls ? this.network.rpcUrls[0] : undefined)
 		);
+
 		this.provider.listAccounts().then((accounts) => console.log(accounts));
 		this.signer = this.provider.getSigner();
+
+		this.state.update((current) => ({
+			...current,
+			chainId: this.network.chainId,
+			correctChain: true
+		}));
+	}
+
+	changeNetwork(network: INetwork | string | 'default' = 'default') {
+		try {
+			const net =
+				typeof network === 'string'
+					? network === 'default'
+						? this.network
+						: networks.find((item) => item.chainId === network)
+					: network;
+			this.provider = <ethers.providers.JsonRpcProvider>(
+				ethers.getDefaultProvider(net?.rpcUrls ? net?.rpcUrls[0] : undefined)
+			);
+			this.state.update((current) => ({
+				...current,
+				chainId: this.network.chainId,
+				correctChain: true
+			}));
+		} catch (err: unknown) {
+			console.log(err);
+		}
 	}
 }
